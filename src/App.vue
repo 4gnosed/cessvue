8124c7dec2502f9932066d5b76047726543ff918
4gnosed <gnosed@foxmail.com>
2020-04-18 19:43:51 +0800

    单页面背景色

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
-<!--    <img src="./assets/logo.png">-->
+    <!--    <img src="./assets/logo.png">-->
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App',
+    beforeCreate() {
+      document.querySelector('body').setAttribute('style', 'background:#F6F6F8')
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 100%;
-}
-/* 解决element-ui的table表格控件表头与内容列不对齐问题 */
-.el-table th.gutter{
-  display: table-cell!important;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+  }
+
+  /* 解决element-ui的table表格控件表头与内容列不对齐问题 */
+  .el-table th.gutter {
+    display: table-cell !important;
+  }
 </style>
